c7c4e8ab075761133aebe149b7cc8b3818428ed9
tableonthewall <seydaozdemir07@gmail.com>
2022-01-18 12:34:29 +0300

    Initial commit

---
+++ basic/src/main/java/com/turkeyjava/security/basic/BasicSecurityApp.java
@@ -0,0 +1,11 @@
+package com.turkeyjava.security.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSecurityApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSecurityApp.class,args);
+    }
+}
