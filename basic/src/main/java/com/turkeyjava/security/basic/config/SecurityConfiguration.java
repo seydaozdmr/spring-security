c8e579d336773add4441c6041cddea0a009e61d8
tableonthewall <seydaozdemir07@gmail.com>
2022-01-19 18:19:36 +0300

    authentication from h2 db with default schema

--- basic/src/main/java/com/turkeyjava/security/basic/config/SecurityConfiguration.java
+++ basic/src/main/java/com/turkeyjava/security/basic/config/SecurityConfiguration.java
@@ -1,33 +1,47 @@
 package com.turkeyjava.security.basic.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //Set your configuration on the auth object. Custom Authentication Manager
-        auth.inMemoryAuthentication()
-                .withUser("turkeyjava")
-                .password("community")
-                .roles("USER")
-        .and()
-        .withUser("test")
-        .password("admin")
-        .roles("ADMIN")
-        .and()
-        .withUser("herkes")
-        .password("herkes")
-        .roles("HERKES");
+//        auth.inMemoryAuthentication()
+//                .withUser("turkeyjava")
+//                .password("community")
+//                .roles("USER")
+//        .and()
+//        .withUser("test")
+//        .password("admin")
+//        .roles("ADMIN")
+//        .and()
+//        .withUser("herkes")
+//        .password("herkes")
+//        .roles("HERKES");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(User.withUsername("admin").password("test").roles("ADMIN"))
+                .withUser(User.withUsername("user").password("test").roles("USER"));
     }
+
 
     @Bean
     public PasswordEncoder encoder(){
@@ -36,12 +50,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/").permitAll() // static içeriği herkes görebilsin diye
+//                .antMatchers("/admin/**").hasRole("ADMIN") //most restrictive to least restrictive
+//                .antMatchers("/user/**").hasAnyRole("USER","ADMIN")
+//                .antMatchers("/hello/**").hasAnyRole("HERKES","USER")
+//                .anyRequest().authenticated() //yetkilendirilmiş herkes bütün pathlara girebilir
+//        .and().formLogin();
         http.authorizeRequests()
-                .antMatchers("/").permitAll() // static içeriği herkes görebilsin diye
-                .antMatchers("/admin/**").hasRole("ADMIN") //most restrictive to least restrictive
-                .antMatchers("/user/**").hasAnyRole("USER","ADMIN")
-                .antMatchers("/hello/**").hasAnyRole("HERKES","USER")
-                .anyRequest().authenticated() //yetkilendirilmiş herkes bütün pathlara girebilir
-        .and().formLogin();
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("ADMIN","USER")
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/**").permitAll()
+                .and().formLogin();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 }
