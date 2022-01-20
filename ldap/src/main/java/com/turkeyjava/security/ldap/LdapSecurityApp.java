b238edb04a8a0b7863be87d7270648e0f4c365e2
tableonthewall <seydaozdemir07@gmail.com>
2022-01-20 18:10:48 +0300

    ldap embedded ldap server

---
+++ ldap/src/main/java/com/turkeyjava/security/ldap/LdapSecurityApp.java
@@ -0,0 +1,11 @@
+package com.turkeyjava.security.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapSecurityApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LdapSecurityApp.class,args);
+    }
+}
---
+++ ldap/src/main/java/com/turkeyjava/security/ldap/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.turkeyjava.security.ldap.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "hello java world";
+    }
+}
