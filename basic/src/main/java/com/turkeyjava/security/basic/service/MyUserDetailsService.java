b63868c44a65183ca150e6f931b8e2f52a0c3667
tableonthewall <seydaozdemir07@gmail.com>
2022-02-03 23:41:36 +0300

    MyUserDetails

--- basic/src/main/java/com/turkeyjava/security/basic/service/MyUserDetailsService.java
+++ basic/src/main/java/com/turkeyjava/security/basic/service/MyUserDetailsService.java
@@ -3,13 +3,10 @@ package com.turkeyjava.security.basic.service;
 import com.turkeyjava.security.basic.model.MyUserDetails;
 import com.turkeyjava.security.basic.model.User;
 import com.turkeyjava.security.basic.repository.UserRepository;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Collection;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
