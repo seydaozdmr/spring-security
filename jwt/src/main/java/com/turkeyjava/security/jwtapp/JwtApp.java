1efc6e255b904b5b1664f6a29d3f3d7c4ea2f100
tableonthewall <seydaozdemir07@gmail.com>
2022-02-03 23:41:45 +0300

    jwt app init

---
+++ jwt/src/main/java/com/turkeyjava/security/jwtapp/JwtApp.java
@@ -0,0 +1,11 @@
+package com.turkeyjava.security.jwtapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtApp {
+    public static void main(String[] args) {
+        SpringApplication.run(JwtApp.class,args);
+    }
+}
