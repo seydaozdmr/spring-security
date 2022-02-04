8125ddd41d1b3cdd7131fc9d03f9562d5554d67b
tableonthewall <seydaozdemir07@gmail.com>
2022-02-04 14:40:41 +0300

    MyUserDetailsService connection to UserRepository

---
+++ jwt/src/main/java/com/turkeyjava/security/jwtapp/service/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.turkeyjava.security.jwtapp.service;
+
+import com.turkeyjava.security.jwtapp.model.MyUserDetails;
+import com.turkeyjava.security.jwtapp.model.User;
+import com.turkeyjava.security.jwtapp.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user=userRepository.findByUserName(username).orElseThrow(()->new RuntimeException("kullanıcı bulunamadı"));
+        return new MyUserDetails(user);
+    }
+}
