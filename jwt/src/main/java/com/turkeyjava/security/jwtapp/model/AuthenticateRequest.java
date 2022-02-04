8f708078398b16f30971948810ab3e53b60fd326
tableonthewall <seydaozdemir07@gmail.com>
2022-02-04 12:19:39 +0300

    Authentication models

---
+++ jwt/src/main/java/com/turkeyjava/security/jwtapp/model/AuthenticateRequest.java
@@ -0,0 +1,30 @@
+package com.turkeyjava.security.jwtapp.model;
+
+public class AuthenticateRequest {
+    private String userName;
+    private String password;
+
+    public AuthenticateRequest(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+    //for serialization
+    public AuthenticateRequest() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ jwt/src/main/java/com/turkeyjava/security/jwtapp/model/AuthenticateResponse.java
@@ -0,0 +1,13 @@
+package com.turkeyjava.security.jwtapp.model;
+
+public class AuthenticateResponse {
+    private final String jwt;
+
+    public AuthenticateResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
