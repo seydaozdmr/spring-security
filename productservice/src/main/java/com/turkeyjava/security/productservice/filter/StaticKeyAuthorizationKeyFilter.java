85f44d39ed3d81ff7d7b8e1c98b4493cadafa75c
tableonthewall <seydaozdemir07@gmail.com>
2022-03-02 09:24:18 +0300

    static key authentication filter

---
+++ productservice/src/main/java/com/turkeyjava/security/productservice/filter/StaticKeyAuthorizationKeyFilter.java
@@ -0,0 +1,38 @@
+package com.turkeyjava.security.productservice.filter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+//@PropertySource("classpath:application.properties")
+public class StaticKeyAuthorizationKeyFilter implements Filter {
+
+    @Value("${authorization.key}")
+    private String authorizationKey;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        var httpRequest=(HttpServletRequest) servletRequest;
+        var httpResponse=(HttpServletResponse) servletResponse;
+
+        String authorization=httpRequest.getHeader("Authorization");
+
+        if(authorizationKey.equals(authorization)){
+            System.out.println("Success authentication");
+            filterChain.doFilter(servletRequest,servletResponse);
+        }else{
+            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+
+    }
+}
