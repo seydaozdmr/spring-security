08ce6f92c51b4f9cff97eda7fa69981c3528b9a2
tableonthewall <seydaozdemir07@gmail.com>
2022-03-02 09:24:37 +0300

    custom filters

---
+++ productservice/src/main/java/com/turkeyjava/security/productservice/filter/AuthenticationLoggingFilter.java
@@ -0,0 +1,21 @@
+package com.turkeyjava.security.productservice.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class AuthenticationLoggingFilter implements Filter {
+    private final Logger logger=Logger.getLogger(AuthenticationLoggingFilter.class.getName());
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        var httpRequest = (HttpServletRequest) servletRequest;
+
+        var requestId= httpRequest.getHeader("Request-Id");
+        logger.info("Successfully authenticated request with id "+ requestId);
+
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+}
---
+++ productservice/src/main/java/com/turkeyjava/security/productservice/filter/RequestValidationFilter.java
@@ -0,0 +1,24 @@
+package com.turkeyjava.security.productservice.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.http.HttpResponse;
+
+public class RequestValidationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        var httpRequest=(HttpServletRequest) servletRequest;
+        var httpResponse=(HttpServletResponse) servletResponse;
+
+        String requestId=httpRequest.getHeader("Request-Id");
+        if(requestId == null || requestId.isBlank()){
+            httpResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+}
