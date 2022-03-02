290115fbdc119d214b52c3dfcdc69a362b0e613f
tableonthewall <seydaozdemir07@gmail.com>
2022-03-02 09:25:15 +0300

    implementing custom filters

--- productservice/src/main/java/com/turkeyjava/security/productservice/config/CustomSecurityConfig.java
+++ productservice/src/main/java/com/turkeyjava/security/productservice/config/CustomSecurityConfig.java
@@ -1,20 +1,27 @@
 package com.turkeyjava.security.productservice.config;
 
+import com.turkeyjava.security.productservice.filter.AuthenticationLoggingFilter;
+import com.turkeyjava.security.productservice.filter.RequestValidationFilter;
+import com.turkeyjava.security.productservice.filter.StaticKeyAuthorizationKeyFilter;
 import com.turkeyjava.security.productservice.service.CustomAuthenticationProvider;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-
-import java.util.ArrayList;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @EnableWebSecurity
 public class CustomSecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomAuthenticationProvider customAuthenticationProvider;
-
-    public CustomSecurityConfig(CustomAuthenticationProvider customAuthenticationProvider) {
+    /**
+     * Bu filtrenin application.properties dosyasındaki veriyi alabilmesi için bean olarak yaratılması
+     * ve run time'de kullanılması gerekiyor.
+     */
+    private final StaticKeyAuthorizationKeyFilter filter;
+    public CustomSecurityConfig(CustomAuthenticationProvider customAuthenticationProvider, StaticKeyAuthorizationKeyFilter filter) {
         this.customAuthenticationProvider = customAuthenticationProvider;
+        this.filter = filter;
     }
 
 
@@ -32,22 +39,51 @@ public class CustomSecurityConfig extends WebSecurityConfigurerAdapter {
          */
         http
                 .authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasRole("USER")
-                .anyRequest().authenticated()
+                .anyRequest().permitAll();
+                //.antMatchers(HttpMethod.POST,"/admin").hasRole("ADMIN")
+                /**
+                 * IMPORTANT To say it again: I recommend and prefer MVC matchers.
+                 * Using MVC matchers, you avoid some of the risks involved with
+                 * the way Spring maps paths to actions. This is because you know
+                 * that the way paths are interpreted for the authorization rules
+                 * are the same as Spring itself interprets these for mapping the
+                 * paths to endpoints. When you use Ant matchers, exercise caution
+                 * and make sure your expressions indeed match everything for which
+                 * you need to apply authorization rules.
+                 */
+                //.antMatchers("/admin").hasRole("ADMIN")
+                //.antMatchers("/user").hasRole("USER")
+                //.anyRequest().authenticated();
+
                 //.anyRequest()
                 //.hasRole("ADMIN")
                 //.access("hasAuthority('READ') and !hasAuthority('DELETE')")
                 //.hasAuthority("READ")
                 //.authenticated()
-                .and()
-                .formLogin()
-                .defaultSuccessUrl("/home",true);
+                //.and()
+                //.formLogin()
+                //.defaultSuccessUrl("/home",true)
+        //.and().httpBasic();
 
         http.csrf().disable();
         http.headers()
                 .frameOptions()
                 .sameOrigin();
+        /**
+         * Configuring the custom filter before authentication
+         */
+        http.addFilterBefore(new RequestValidationFilter(),
+                BasicAuthenticationFilter.class);
+        /**
+         * Configuring the custom filter after authentication
+         */
+        http.addFilterAfter(new AuthenticationLoggingFilter(),
+                BasicAuthenticationFilter.class);
+        /**
+         * Configuring the custom filter instead basic authentication filter
+         */
+        http.addFilterAt(filter,
+                BasicAuthenticationFilter.class);
     }
 
 //    @Bean
